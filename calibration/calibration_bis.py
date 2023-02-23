b5574caedc96800f35eb9fbd21cbb82063626a1d
Jay-boo <jeanalainpro2@gmail.com>
2023-02-23 11:43:40 +0100

    res

--- calibration/calibration_bis.py
+++ calibration/calibration_bis.py
@@ -28,7 +28,10 @@ imgpointsR = [] # 2d points in image plane.
 
 imagesLeft = sorted(glob.glob('../undistort/fish/*.png'))
 imagesRight = sorted(glob.glob('img_calib/img_infra/*.png'))
+print(imagesLeft)
+print(imagesRight)
 
+counter=0
 for imgLeft, imgRight in zip(imagesLeft, imagesRight):
 
     imgL = cv.imread(imgLeft)
@@ -43,6 +46,7 @@ for imgLeft, imgRight in zip(imagesLeft, imagesRight):
     # If found, add object points, image points (after refining them)
     if retL and retR == True:
 
+
         objpoints.append(objp)
 
         cornersL = cv.cornerSubPix(grayL, cornersL, (11,11), (-1,-1), criteria)
@@ -53,10 +57,16 @@ for imgLeft, imgRight in zip(imagesLeft, imagesRight):
 
         # Draw and display the corners
         cv.drawChessboardCorners(imgL, chessboardSize, cornersL, retL)
-        cv.imshow('img left', imgL)
+        cv.imshow('fisheye', imgL)
+        cv.imwrite(f'draw/fish/imgFISH{counter}.png',imgL)
         cv.drawChessboardCorners(imgR, chessboardSize, cornersR, retR)
         cv.imshow('img right', imgR)
+        cv.imwrite(f'draw/infra/imgINF{counter}.png',imgR)
         cv.waitKey(1000)
+        counter+=1
+    else:
+        print("NOt ok retL and retR")
+        print(imgLeft,imgRight)
 
 
 cv.destroyAllWindows()
@@ -65,6 +75,7 @@ cv.destroyAllWindows()
 
 
 ############## CALIBRATION #######################################################
+print(len(objpoints))
 
 retL, cameraMatrixL, distL, rvecsL, tvecsL = cv.calibrateCamera(objpoints, imgpointsL, frameSize, None, None)
 heightL, widthL, channelsL = imgL.shape
@@ -73,18 +84,68 @@ newCameraMatrixL, roi_L = cv.getOptimalNewCameraMatrix(cameraMatrixL, distL, (wi
 retR, cameraMatrixR, distR, rvecsR, tvecsR = cv.calibrateCamera(objpoints, imgpointsR, frameSize, None, None)
 heightR, widthR, channelsR = imgR.shape
 newCameraMatrixR, roi_R = cv.getOptimalNewCameraMatrix(cameraMatrixR, distR, (widthR, heightR), 1, (widthR, heightR))
+print('-------------------individual cam calib')
+print("FISHEYE")
+print(retL)
+print("INFRA")
+print(retR)
 ########## Stereo Vision Calibration #############################################
 
 flags = 0
 flags |= cv.CALIB_FIX_INTRINSIC
-# Here we fix the intrinsic camara matrixes so that only Rot, Trns, Emat and Fmat are calculated.
-# Hence intrinsic parameters are the same 
 
 criteria_stereo= (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # This step is performed to transformation between the two cameras and calculate Essential and Fundamenatl matrix
 retStereo, newCameraMatrixL, distL, newCameraMatrixR, distR, rot, trans, essentialMatrix, fundamentalMatrix = cv.stereoCalibrate(objpoints, imgpointsL, imgpointsR, newCameraMatrixL, distL, newCameraMatrixR, distR, grayL.shape[::-1], criteria_stereo, flags)
 
+def calc_rms_stereo(objectpoints, imgpoints_l, imgpoints_r, A1, D1, A2, D2, R, T):
+    tot_error = 0
+    total_points = 0
+
+    for i, objpoints in enumerate(objectpoints):
+        print(f"-------------------- {i} ----------")
+        # calculate world <-> cam1 transformation
+        _, rvec_l, tvec_l,_ = cv.solvePnPRansac(objpoints, imgpoints_l[i], A1, D1)
+
+        # compute reprojection error for cam1
+        rp_l, _ = cv.projectPoints(objpoints, rvec_l, tvec_l, A1, D1)
+        print("fisheye reprojection error")
+        print(np.sum(np.square(np.float64(imgpoints_l[i] - rp_l))))
+        tot_error += np.sum(np.square(np.float64(imgpoints_l[i] - rp_l)))
+        total_points += len(objpoints)
+
+        # calculate world <-> cam2 transformation
+        # rvec_r, tvec_r  = cv.composeRT(rvec_l,tvec_l,cv.Rodrigues(R)[0],T)[:2]
+        _, rvec_r, tvec_r,_ = cv.solvePnPRansac(objpoints, imgpoints_r[i], A2, D2)
+
+        # compute reprojection error for cam2
+        rp_r,_ = cv.projectPoints(objpoints, rvec_r, tvec_r, A2, D2)
+        tot_error += np.square(imgpoints_r[i] - rp_r).sum()
+        print("infra reprojection error")
+        print(np.square(imgpoints_r[i] - rp_r).sum())
+        total_points += len(objpoints)
+
+    mean_error = np.sqrt(tot_error/total_points)
+
+    return mean_error
+
+res_1=calc_rms_stereo(
+    objpoints,
+    imgpointsL,
+    imgpointsR,
+    newCameraMatrixL,
+    distL,
+    newCameraMatrixR,
+    distR,
+    rot,
+    trans
+)
+
+
+print(f"custom stereo calibration rmse :{res_1}")
+print(res_1)
+print(f"built in sterroCalibration rmse : {retStereo}")
 
 
 
