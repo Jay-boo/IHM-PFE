ab4e74cc5779237134843f8892d36efef4f737b8
Jay-boo <jeanalainpro2@gmail.com>
2023-03-06 12:24:57 +0100

    ensure working

--- calibration/calibration.py
+++ calibration/calibration.py
@@ -20,8 +20,8 @@ def calibrate_camera(chessboardSize=(8,6),img_calib_dir="img_calib/"):
     imgpointsL = [] # 2d points in image plane.
     imgpointsR = [] # 2d points in image plane.
 
-    imagesFISH = sorted(glob.glob(img_calib_dir+'img_calib/img_fisheye/*.png'))
-    imagesINFRA = sorted(glob.glob( img_calib_dir+'img_infra/*.png'))
+    imagesFISH = sorted(glob.glob(img_calib_dir+'img_fisheye_individual/*.png'))
+    imagesINFRA = sorted(glob.glob( img_calib_dir+'img_infra_stereo/*.png'))
     print(imagesINFRA,imagesFISH)
 
     for imgLeft, imgRight in zip(imagesFISH, imagesINFRA):
@@ -71,9 +71,8 @@ def calibrate_camera(chessboardSize=(8,6),img_calib_dir="img_calib/"):
     rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
     tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
 
-    calibration_flags = cv.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv.fisheye.CALIB_CHECK_COND+cv.fisheye.CALIB_FIX_SKEW
-    rms, _, _, _, _ = \
-        cv.fisheye.calibrate(
+    calibration_flags = cv.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv.fisheye.CALIB_FIX_SKEW
+    rms, _, _, _, _ =cv.fisheye.calibrate(
             objpoints,
             imgpointsL,
             grayL.shape[::-1],
@@ -83,7 +82,7 @@ def calibrate_camera(chessboardSize=(8,6),img_calib_dir="img_calib/"):
             tvecs,
             calibration_flags,
             (cv.TERM_CRITERIA_EPS+cv.TERM_CRITERIA_MAX_ITER, 30, 1e-6)
-        )
+    )
 
     final_K= cv.fisheye.estimateNewCameraMatrixForUndistortRectify(K,
                                                                      D,
@@ -116,14 +115,14 @@ def calibrate_camera(chessboardSize=(8,6),img_calib_dir="img_calib/"):
         map_1,map_2=cv.fisheye.initUndistortRectifyMap(K,D,np.eye(3),K,frameSize,cv.CV_16SC2)
         undistorted_img=cv.remap(img,map_1,map_2,interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
         cv.imshow("frame undistorted",undistorted_img)
-        cv.imwrite(f'../undistort/fish/image_{counter}.png',undistorted_img)
+        cv.imwrite(f'undistort/fish/imgFISH{counter}.png',undistorted_img)
 
         img = cv.imread(imgRight)
         cv.imshow("frame Infra",img)
         map_1,map_2=cv.initUndistortRectifyMap(cameraMatrixR,distR,None,newCameraMatrixR,frameSize,cv.CV_32FC1)
         undistorted_img=cv.remap(img,map_1,map_2,interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
         cv.imshow("frame undistorted",undistorted_img)
-        cv.imwrite(f'../undistort/infra/image_{counter}.png',undistorted_img)
+        cv.imwrite(f'undistort/infra/imgINF{counter}.png',undistorted_img)
         cv.waitKey(1000)
         counter+=1
     cv.destroyAllWindows()
